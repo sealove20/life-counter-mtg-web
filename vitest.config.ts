d0a0b13bdfdbd6948fbefe9e6f0b770e9060584c
Matheus Henrique <matheus.181137@gmail.com>
2025-04-05 12:52:32 -0300

    chore: fix react test import

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vitest/config";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    test: {
-        globals: true,
-        environment: "jsdom",
-        setupFiles: "./setupTests.ts",
-        alias: {
-            "@store": path.resolve(__dirname, "src/store"),
-        },
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    alias: {
+      "@store": path.resolve(__dirname, "src/store"),
     },
+  },
 });
